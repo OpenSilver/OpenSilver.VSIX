5098d02d6ddb2225c64c1b73893a69e1b7039b70
Fabio Silva <fabio.silva@userware.onmicrosoft.com>
2023-10-11 09:59:56 -0300

    feat: OpenRia Business Template

---
+++ src/OpenSilverBusinessVBApplicationTemplate/OpenSilverBusinessApplication/Views/About.xaml.vb
@@ -0,0 +1,18 @@
+﻿Imports System.Windows.Controls
+Imports System.Windows.Navigation
+
+Namespace $ext_safeprojectname$.Views
+
+    Partial Public Class About
+        Inherits Page
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+
+        Protected Overrides Sub OnNavigatedTo(e As NavigationEventArgs)
+        End Sub
+
+    End Class
+
+End Namespace
---
+++ src/OpenSilverBusinessVBApplicationTemplate/OpenSilverBusinessApplication/Views/Home.xaml.vb
@@ -0,0 +1,18 @@
+﻿Imports System.Windows.Controls
+Imports System.Windows.Navigation
+
+Namespace $ext_safeprojectname$.Views
+
+    Partial Public Class Home
+        Inherits Page
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+
+        Protected Overrides Sub OnNavigatedTo(e As NavigationEventArgs)
+        End Sub
+
+    End Class
+
+End Namespace
