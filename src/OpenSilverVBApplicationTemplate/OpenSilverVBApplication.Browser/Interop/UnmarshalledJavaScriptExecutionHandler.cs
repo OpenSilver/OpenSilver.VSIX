dcf2863ed68e906f22b297a1e57a09895ceafcb2
Iakov Lilo <iakov.lilo@userware.onmicrosoft.com>
2023-10-11 20:51:25 +1100

    Fix interop

--- src/OpenSilverVBApplicationTemplate/OpenSilverVBApplication.Browser/Interop/UnmarshalledJavaScriptExecutionHandler.cs
+++ src/OpenSilverVBApplicationTemplate/OpenSilverVBApplication.Browser/Interop/UnmarshalledJavaScriptExecutionHandler.cs
@@ -5,38 +5,38 @@ using Microsoft.JSInterop.WebAssembly;
 namespace $safeprojectname$.Interop
 {
     public class UnmarshalledJavaScriptExecutionHandler : IWebAssemblyExecutionHandler
-{
-    private const string MethodName = "callJSUnmarshalled";
-    private readonly WebAssemblyJSRuntime _runtime;
-
-    public UnmarshalledJavaScriptExecutionHandler(IJSRuntime runtime)
     {
-        _runtime = runtime as WebAssemblyJSRuntime;
-    }
+        private const string MethodName = "callJSUnmarshalled_v2";
+        private readonly WebAssemblyJSRuntime _runtime;
 
-    public void ExecuteJavaScript(string javaScriptToExecute)
-    {
-        _runtime.InvokeUnmarshalled<string, object>(MethodName, javaScriptToExecute);
-    }
+        public UnmarshalledJavaScriptExecutionHandler(IJSRuntime runtime)
+        {
+            _runtime = runtime as WebAssemblyJSRuntime;
+        }
 
-    public object ExecuteJavaScriptWithResult(string javaScriptToExecute)
-    {
-        return _runtime.InvokeUnmarshalled<string, object>(MethodName, javaScriptToExecute);
-    }
+        public void ExecuteJavaScript(string javaScriptToExecute)
+        {
+            _runtime.InvokeUnmarshalled<string, object>(MethodName, javaScriptToExecute);
+        }
 
-    public TResult InvokeUnmarshalled<T0, TResult>(string identifier, T0 arg0)
-    {
-        return _runtime.InvokeUnmarshalled<T0, TResult>(identifier, arg0);
-    }
+        public object ExecuteJavaScriptWithResult(string javaScriptToExecute)
+        {
+            return _runtime.InvokeUnmarshalled<string, object>(MethodName, javaScriptToExecute);
+        }
 
-    public TResult InvokeUnmarshalled<T0, T1, TResult>(string identifier, T0 arg0, T1 arg1)
-    {
-        return _runtime.InvokeUnmarshalled<T0, T1, TResult>(identifier, arg0, arg1);
-    }
+        public TResult InvokeUnmarshalled<T0, TResult>(string identifier, T0 arg0)
+        {
+            return _runtime.InvokeUnmarshalled<T0, TResult>(identifier, arg0);
+        }
 
-    public TResult InvokeUnmarshalled<T0, T1, T2, TResult>(string identifier, T0 arg0, T1 arg1, T2 arg2)
-    {
-        return _runtime.InvokeUnmarshalled<T0, T1, T2, TResult>(identifier, arg0, arg1, arg2);
+        public TResult InvokeUnmarshalled<T0, T1, TResult>(string identifier, T0 arg0, T1 arg1)
+        {
+            return _runtime.InvokeUnmarshalled<T0, T1, TResult>(identifier, arg0, arg1);
+        }
+
+        public TResult InvokeUnmarshalled<T0, T1, T2, TResult>(string identifier, T0 arg0, T1 arg1, T2 arg2)
+        {
+            return _runtime.InvokeUnmarshalled<T0, T1, T2, TResult>(identifier, arg0, arg1, arg2);
+        }
     }
-}
 }
