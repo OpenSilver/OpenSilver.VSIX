e0fe0b1f2091dc9d81a5d02c94ff6f4545206ebe
Iakov Lilo <iakov.lilo@userware.onmicrosoft.com>
2024-04-11 17:51:20 +0600

    Update Xaml Designer package

--- src/OpenSilver.XamlDesigner/ProductConfig.cs
+++ src/OpenSilver.XamlDesigner/ProductConfig.cs
@@ -5,5 +5,7 @@ namespace OpenSilver.XamlDesigner
     internal class ProductConfig : IProductConfig
     {
         public string Name => "OpenSilver";
+
+        public string Slug => "opensilver";
     }
 }
