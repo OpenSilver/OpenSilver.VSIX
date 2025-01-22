c9dcecabee1fccee8344e3565f88b0520f72d360
Iakov Lilo <iakov.lilo@userware.onmicrosoft.com>
2025-01-22 16:36:53 +1100

    Add a template for Maui Hybrid project

---
+++ src/OpenSilver.CLI/CommonProjectTemplates/OpenSilverApplication.MauiHybrid/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace OpenSilverApplication.MauiHybrid;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+}
---
+++ src/OpenSilver.CLI/CommonProjectTemplates/OpenSilverApplication.MauiHybrid/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace OpenSilverApplication.MauiHybrid;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
