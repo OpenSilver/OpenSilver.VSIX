a968c6526b49181e62b0b0776f3ed560eb0592de
Andrei Arekhva <a.arekhva@gmail.com>
2025-02-11 14:07:14 +0300

    Remove ".MauiHybrid" from the default title of MAUI app for Windows

--- src/OpenSilverMauiHybridTemplate/App.xaml.cs
+++ src/OpenSilverMauiHybridTemplate/App.xaml.cs
@@ -9,6 +9,6 @@ public partial class App : Application
 
     protected override Window CreateWindow(IActivationState? activationState)
     {
-        return new Window(new MainPage()) { Title = "$safeprojectname$" };
+        return new Window(new MainPage()) { Title = "$rootprojectname$" };
     }
 }
