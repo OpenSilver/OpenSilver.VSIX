fbedb6d2db5523b6f1e6369eec95bda56ae2c14f
Iakov Lilo <iakov.lilo@userware.onmicrosoft.com>
2025-02-12 10:45:48 +1100

    Adjust title for CLI template

--- src/OpenSilver.CLI/CommonProjectTemplates/OpenSilverApplication.MauiHybrid/App.xaml.cs
+++ src/OpenSilver.CLI/CommonProjectTemplates/OpenSilverApplication.MauiHybrid/App.xaml.cs
@@ -9,6 +9,6 @@ public partial class App : Application
 
     protected override Window CreateWindow(IActivationState? activationState)
     {
-        return new Window(new MainPage()) { Title = "OpenSilverApplication.MauiHybrid" };
+        return new Window(new MainPage()) { Title = "OpenSilverApplication" };
     }
 }
