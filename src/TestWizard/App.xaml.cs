2591fa545806fb12bb0d5ff3363c694f72309e49
Andrei Arekhva <a.arekhva@gmail.com>
2025-03-13 12:27:01 +0300

    Add a WPF project to test wizard dialogs

---
+++ src/TestWizard/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using OpenSilver.TemplateWizards.AppCustomizationWindow;
+using System.Windows;
+
+namespace TestWizard;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+    public App()
+    {
+        MainWindow = new Window();
+
+        var configurationWindow = new AppConfigurationWindow();
+        _ = configurationWindow.ShowDialog();
+
+        MainWindow.Close();
+    }
+}
+
