d2d69a3d73344c5c91f48e395d1bccd5a3acbd9e
Iakov Lilo <iakov.lilo@userware.onmicrosoft.com>
2025-03-17 18:42:00 +1100

    Update versions, adjust MAUI Hybrid templates

--- src/OpenSilver.TemplateWizards/Shared/GlobalConstants.cs
+++ src/OpenSilver.TemplateWizards/Shared/GlobalConstants.cs
@@ -6,16 +6,16 @@ namespace OpenSilver.TemplateWizards.Shared
     /// </summary>
     public static class GlobalConstants
     {
-        public const string OpenSilverMauiHybridPackageVersion = "3.2.0-preview-2025-01-31-144842-4edde0a3";
+        public const string OpenSilverMauiHybridPackageVersion = "3.2.0";
 
-        public const string OpenSilverPackageVersion = "3.1.2";
+        public const string OpenSilverPackageVersion = "3.2.0";
 
-        public const string OpenSilverSimulatorPackageVersion = "3.1.2";
+        public const string OpenSilverSimulatorPackageVersion = "3.2.0";
 
-        public const string OpenSilverWebAssemblyPackageVersion = "3.1.2";
+        public const string OpenSilverWebAssemblyPackageVersion = "3.2.0";
 
-        public const string OpenRia46PackageVersion = "3.1.0";
+        public const string OpenRia46PackageVersion = "3.2.0";
 
-        public const string OpenSilverThemeModernPackageVersion = "3.1.*";
+        public const string OpenSilverThemeModernPackageVersion = "3.2.*";
     }
 }
