9ba584b04558c2ae7c0ff912aff777cb29f2256f
Iakov Lilo <iakov.lilo@userware.onmicrosoft.com>
2025-05-28 11:48:31 +0600

    Fix MAUI iOS safe area to prevent status bar overlap

--- src/OpenSilver.CLI/CommonProjectTemplates/OpenSilverApplication.MauiHybrid/MainPage.xaml.cs
+++ src/OpenSilver.CLI/CommonProjectTemplates/OpenSilverApplication.MauiHybrid/MainPage.xaml.cs
@@ -1,9 +1,15 @@
-﻿namespace OpenSilverApplication.MauiHybrid;
+﻿using Microsoft.Maui.Controls.PlatformConfiguration;
+using Microsoft.Maui.Controls.PlatformConfiguration.iOSSpecific;
+
+namespace OpenSilverApplication.MauiHybrid;
 
 public partial class MainPage : ContentPage
 {
     public MainPage()
     {
         InitializeComponent();
+
+        // Prevents content from overlapping the status bar on iOS
+        On<iOS>().SetUseSafeArea(true);
     }
 }
--- src/OpenSilverMauiHybridTemplate/MainPage.xaml.cs
+++ src/OpenSilverMauiHybridTemplate/MainPage.xaml.cs
@@ -1,9 +1,15 @@
-﻿namespace $safeprojectname$;
+﻿using Microsoft.Maui.Controls.PlatformConfiguration;
+using Microsoft.Maui.Controls.PlatformConfiguration.iOSSpecific;
+
+namespace $safeprojectname$;
 
 public partial class MainPage : ContentPage
 {
     public MainPage()
     {
         InitializeComponent();
+
+        // Prevents content from overlapping the status bar on iOS
+        On<iOS>().SetUseSafeArea(true);
     }
 }
