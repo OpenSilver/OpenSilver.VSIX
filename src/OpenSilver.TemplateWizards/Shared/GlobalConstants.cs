260f26ef135d86231740557a928f273ec40800d9
Iakov Lilo <iakov.lilo@userware.onmicrosoft.com>
2025-06-04 13:43:59 +0600

    Update versions

--- src/OpenSilver.TemplateWizards/Shared/GlobalConstants.cs
+++ src/OpenSilver.TemplateWizards/Shared/GlobalConstants.cs
@@ -6,13 +6,13 @@ namespace OpenSilver.TemplateWizards.Shared
     /// </summary>
     public static class GlobalConstants
     {
-        public const string OpenSilverMauiHybridPackageVersion = "3.2.0";
+        public const string OpenSilverMauiHybridPackageVersion = "3.2.2";
 
-        public const string OpenSilverPackageVersion = "3.2.0";
+        public const string OpenSilverPackageVersion = "3.2.2";
 
-        public const string OpenSilverSimulatorPackageVersion = "3.2.0";
+        public const string OpenSilverSimulatorPackageVersion = "3.2.2";
 
-        public const string OpenSilverWebAssemblyPackageVersion = "3.2.0";
+        public const string OpenSilverWebAssemblyPackageVersion = "3.2.2";
 
         public const string OpenRia46PackageVersion = "3.2.0";
 
